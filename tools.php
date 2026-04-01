b40f0208158da305b6be33ea2f7a910268a43dba
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-04-01 20:11:58 +0200

    tools

--- tools.php
+++ tools.php
@@ -5,8 +5,8 @@ function displayBanner($title = "") {
     }
     
     $output = '
-    <section class="py-20 banner-bg mt-8 rounded-2xl">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+    <section class=" py-20 banner-bg mt-8 ">
+        <div class="px-4 sm:px-6 lg:px-8 text-center">
             <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-white">
                 ' . $title . '
             </h1>
