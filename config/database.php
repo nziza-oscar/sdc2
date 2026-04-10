b5322ed50be5c56901101cc0706f27788a4cfded
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-04-10 10:16:58 +0200

    feat: config/database.php

--- config/database.php
+++ config/database.php
@@ -4,6 +4,11 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'sdcc_cms');
 
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'uwsmilab_root');
+// define('DB_PASS', 'Y9%7pSCia=8#');
+// define('DB_NAME', 'uwsmilab_sdcc_cms');
+
 function getDB() {
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
